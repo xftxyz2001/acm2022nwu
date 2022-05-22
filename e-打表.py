62bb173ccfdf84e55ff945678c631c7c344f26eb
通晓宇宙 <2581011320@qq.com>
2022-05-23 00:14:06 +0800

    e

--- e-打表.py
+++ e-打表.py
@@ -3,7 +3,7 @@ import math
 prime_dict = {}
 
 
-def is_prime(number):
+def ip(number):
     if number > 1:
         if number == 2:
             return True
@@ -16,25 +16,18 @@ def is_prime(number):
     return False
 
 
-def factor(n):
-    result = []
-    for x in range(1, int(math.sqrt(n) + 1)):
-        if n % x == 0:
-            y = n // x
-            result.append(x)
-            result.append(y)
-    return result
+def is_prime(number):
+    if number not in prime_dict:
+        prime_dict[number] = ip(number)
+    return prime_dict[number]
 
 
 def check(n):
-    check_list = [i + n / i for i in factor(n)]
-    for i in check_list:
-        if i in prime_dict:
-            return prime_dict[i]
-        if not is_prime(i):
-            prime_dict[i] = False
-            return False
-    prime_dict[i] = True
+    for x in range(1, int(math.sqrt(n) + 1)):
+        if n % x == 0:
+            y = n // x
+            if not is_prime(x + n // x) or not is_prime(y + n // y):
+                return False
     return True
 
 
